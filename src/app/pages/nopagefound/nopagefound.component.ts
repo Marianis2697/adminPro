1a9b685342cdc3c3090f02a45fd8a18c1f390838
Mariana Colio <marian.mkm3@gmail.com>
2024-08-28 11:31:27 -0600

    adminPro

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styleUrls:['./error-pages.css']
+})
+export class NopagefoundComponent {
+  year = new Date().getFullYear();
+
+}
